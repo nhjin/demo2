336fc0f100c291bbf24f2dad3eae61f135e62b4f
nhjin <nhjin1106@gmail.com>
2021-05-24 14:32:20 +0900

    init

--- src/main/java/com/example/demo2/firebase/FirebaseInitialization.java
+++ src/main/java/com/example/demo2/firebase/FirebaseInitialization.java
@@ -35,20 +35,6 @@ public class FirebaseInitialization {
                 e.printStackTrace();
             }
 
-//        FileInputStream serviceAccount = null;
-//        try {
-//           // new FileInputStream("./serviceAccountKey.json");
-//
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .build();
-//
-//            FirebaseApp.initializeApp(options);
-//
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        }
-
     }
 
 }
