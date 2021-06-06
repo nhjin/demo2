9b88f645664bd875d4ac3cb915bc56213f0ac09a
temeraire97 <hyensooyoon@gmail.com>
2021-06-06 13:03:30 +0900

    윤현수 업데이트

--- src/main/java/com/example/demo2/controller/ProductController.java
+++ src/main/java/com/example/demo2/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.example.demo2.controller;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import com.example.demo2.entity.Flex;
 import com.example.demo2.entity.Product;
 import com.example.demo2.service.ProductService;
 
@@ -21,10 +23,22 @@ public class ProductController {
         return productService.saveProduct(product);
     }
 
-    @GetMapping("/products/{name}")
-    public Product getProduct(@PathVariable String name) throws InterruptedException, ExecutionException{
+    @GetMapping("/products/{value}")
+    public List<Flex> getAllProduct(@PathVariable String value) throws InterruptedException, ExecutionException{
 
-        return productService.getProductDetails(name);
+        return productService.getProducts(value);
+    }
+
+    @GetMapping("/products/{value}/{name}")
+    public Flex getProduct(@PathVariable String value, @PathVariable String name) throws InterruptedException, ExecutionException{
+
+        return productService.getProductDetails(value, name);
+    }
+
+    @GetMapping("/products/test/{category}/{id}")
+    public List<Flex> getProductTest(@PathVariable String category, @PathVariable int id) throws InterruptedException, ExecutionException{
+
+        return productService.getProductTest(category, id);
     }
 
 }
--- src/main/java/com/example/demo2/service/ProductService.java
+++ src/main/java/com/example/demo2/service/ProductService.java
@@ -1,22 +1,23 @@
 package com.example.demo2.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import com.example.demo2.entity.Flex;
 import com.example.demo2.entity.Product;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.*;
 import org.springframework.stereotype.Service;
 
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 
 @Service
 public class ProductService {
 
-    private static final String COLLECTION_NAME ="products";
+    private static final String COLLECTION_NAME = "products";
 
     public String saveProduct(Product product) throws InterruptedException, ExecutionException {
         Firestore dbFireFire = FirestoreClient.getFirestore();
@@ -27,23 +28,66 @@ public class ProductService {
 
     }
 
-    public Product getProductDetails(String name) throws ExecutionException, InterruptedException {
+    public List<Flex> getProducts(String value) throws ExecutionException, InterruptedException {
 
         Firestore dbFirestore = FirestoreClient.getFirestore();
 
-        DocumentReference documentReference = dbFirestore.collection(COLLECTION_NAME).document(name);
+        Iterable<DocumentReference> documentReference = dbFirestore.collection(value).listDocuments();
+        Iterator<DocumentReference> iterator = documentReference.iterator();
+
+        List<Flex> productList = new ArrayList<>();
+        Flex product = null;
+
+        while (iterator.hasNext()) {
+            DocumentReference documentReference1 = iterator.next();
+            ApiFuture<DocumentSnapshot> future = documentReference1.get();
+            DocumentSnapshot document = future.get();
+
+            product = document.toObject(Flex.class);
+            productList.add(product);
+        }
+        return productList;
+    }
+
+    public Flex getProductDetails(String value, String name) throws ExecutionException, InterruptedException {
+
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        DocumentReference documentReference = dbFirestore.collection(value).document(name);
 
         ApiFuture<DocumentSnapshot> future = documentReference.get();
 
         DocumentSnapshot document = future.get();
 
-        Product product = null;
-        if(document.exists()){
-            product = document.toObject(Product.class);
-        return product;
-        }else {
+        Flex product = null;
+        if (document.exists()) {
+            product = document.toObject(Flex.class);
+            return product;
+        } else {
             return null;
         }
+    }
+
+    public List<Flex> getProductTest(String category, int id) throws ExecutionException, InterruptedException {
+
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        CollectionReference flexitarian = dbFirestore.collection(category);
+
+        System.out.println(id);
+
+        Query query = flexitarian.orderBy("id", Query.Direction.DESCENDING).startAfter(id).limit(6);
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+        List<Flex> productList = new ArrayList<>();
+        Flex product = null;
+
+        for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+//                System.out.println(document.getId());
+            product = document.toObject(Flex.class);
+            productList.add(product);
+        }
+        return productList;
     }
 
 }
