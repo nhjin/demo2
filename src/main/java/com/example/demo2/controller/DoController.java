2294c76348c0ae7dc465b99253e893b65abae847
temeraire97 <hyensooyoon@gmail.com>
2021-06-10 14:53:30 +0900

    윤현수 업데이트

--- src/main/java/com/example/demo2/controller/DoController.java
+++ src/main/java/com/example/demo2/controller/DoController.java
@@ -35,11 +35,43 @@ public class DoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DoController.class);
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String ajaxTest() {
-        //logger.info("Welcome home! Test!{}");
+        logger.info("Welcome home! Test!{}");
 
         return "index";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+
+        return "test";
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home() {
+//        logger.info("Welcome home! Test!{}");
+
+        return "home";
+    }
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String about() {
+//        logger.info("Welcome home! Test!{}");
+
+        return "about";
+    }
+    @RequestMapping(value = "/recipes", method = RequestMethod.GET)
+    public String recipe() {
+//        logger.info("Welcome home! Test!{}");
+
+        return "shop";
+    }
+    @RequestMapping(value = "/recipes/{id}", method = RequestMethod.GET)
+    public String test2(@PathVariable int id, HttpServletRequest request) {
+//        logger.info("Welcome home! Test!{}");
+        request.setAttribute("id", id);
+
+        return "shopDetail";
     }
 
     //회원가입 구글계정
